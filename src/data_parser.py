c9d12ec45fab079dd9b05067fd327e680f8d68ad
Aleksandra Hein <aleksandra.hein@o2.pl>
2022-06-01 16:56:32 +0200

    loading - in progress

--- src/data_parser.py
+++ src/data_parser.py
@@ -1,16 +1,44 @@
+import json
+import numpy as np
 from sentence_processor import SentenceProcessor
 
 class DataParser():
     def __init__(self):
         self.sp = SentenceProcessor()
         self.bag_of_words = []
+        self.tags = []
+        self.xy = [] # hold both patterns and the text
 
     def load(self, path):
-        # load json
-        # process it using sentence processor
-        # append to bag of words, so that we can call load multiple times, on multiple times, and build our dataset from multiple files
-        pass
+        with open(path, 'r') as f:
+            intents = json.load(f)
+
+        all_words = []
+        for intent in intents['intents']:
+            tag = intent['tag']
+            self.tags.append(tag)
+            for pattern in intent['patterns']:
+                w = self.sp.tokenize(pattern)
+                all_words.extend(w)
+                self.xy.append((w, tag))
+
+        ignore_words = ['?', '!', '.', ',']
+        all_words = [self.sp.stem(w) for w in all_words if w not in ignore_words]
+        all_words = sorted(set(all_words))  # removing duplicates
+        self.tags = sorted(set(self.tags))
+        self.bag_of_words.append(all_words)
 
     def generate_training_data(self):
         # generates xtrain, ytrain to pass into dataset
-        pass
+        x_train = []
+        y_train = []
+
+        for(pattern_sentence, tag) in self.xy:
+            bag = self.sp.bag_of_words(pattern_sentence, tag)
+            x_train.append(bag)
+            label = self.tags.index(tag)
+            y_train.append(label)
+
+        x_train = np.array(x_train)
+        y_train = np.array(y_train)
+
